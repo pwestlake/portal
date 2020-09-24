526b8301ddd73105347c5503922fc67259e45e23
pwestlake <philip@pwestlake.com>
2020-09-24 15:10:46 +0100

    Added authentication

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
 
 function App() {
   return (
     <div className="App">
+      <AmplifySignOut />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -23,4 +25,4 @@ function App() {
   );
 }
 
-export default App;
+export default withAuthenticator(App);
