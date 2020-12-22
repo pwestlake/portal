30ce4500d258ca56374a38379032cf15bf8b6049
pwestlake <philip@pwestlake.com>
2020-12-22 14:26:17 +0000

    Added population field

--- lambda/covid19/region/pkg/domain/region.go
+++ lambda/covid19/region/pkg/domain/region.go
@@ -3,10 +3,11 @@ package domain
 // Region ...
 // Region domain object
 type Region struct {
-	Key     string  `json:"key"`
-	Country string  `json:"country"`
-	State   string  `json:"state"`
-	Lat     float64 `json:"lat"`
-	Long    float64 `json:"long"`
-	Name    string  `json:"name"`
+	Key        string  `json:"key"`
+	Country    string  `json:"country"`
+	State      string  `json:"state"`
+	Lat        float64 `json:"lat"`
+	Long       float64 `json:"long"`
+	Name       string  `json:"name"`
+	Population int     `json:"population"`
 }
